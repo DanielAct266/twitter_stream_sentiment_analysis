b048be272af564033dc3d7be13e58fd63fd8e8c4
DanielAct266 <danielguerra333@gmail.com>
2021-02-22 10:48:40 -0600

    Upgrades tweet_stream

--- tweet_stream.py
+++ tweet_stream.py
@@ -1,11 +1,10 @@
 
 import csv
 import json
+import os
 import sys
 
-import boto3
 import click
-import pandas as pd
 from tweepy import OAuthHandler, Stream, API
 from tweepy.streaming import StreamListener
 
@@ -23,6 +22,12 @@ class TweetsListener(StreamListener):
         Custom tweet logging and extracting.
         Saves tweets extracted in an output csv file
     """
+    def __init__(self, tweet_limit):
+        """
+            Redefining Constructor: adding a limit of tweets to stream.
+        """
+        super().__init__()
+        self.tweet_limit = tweet_limit
 
     def on_status(self, status):
         """
@@ -50,22 +55,19 @@ class TweetsListener(StreamListener):
             text = text.replace(c," ")
             quoted_text = quoted_text.replace(c, " ")
 
+        os.makedirs("data", exist_ok=True)
+
         with open(f"data/out.csv", "a", encoding='utf-8') as out:
             global counter
             counter+=1
             csv_out = csv.writer(out)
             data = (str(status.created_at.date()),
-                    text, status.user.location, is_retweet, is_quote, status.place)
-            print(f"{data}")
+                    text, status.user.location, is_retweet, 
+                    is_quote, status.place)
+            print(data)
             csv_out.writerow(data)
-
-            if counter % 1000 == 0:
-                print("\n ######################")
-                print(f"{counter} tweets extracted")
-                print("######################\n")
             
-            ### In this case the limit of tweets is 100
-            if counter == 100:
+            if counter == self.tweet_limit:
                 print("\n ######################")
                 print("Tweets Limit Reached!")
                 print(f"{counter} tweets extracted")
@@ -80,17 +82,16 @@ class TweetsListener(StreamListener):
         return True
 
 @click.command()
-# @click.option('--limit', prompt='Enter the number of tweets that you want to stream',
-#               help='Must be an integer')
+@click.option('--tweet_limit', prompt='Enter the number of tweets that you want to stream',
+              help='Must be an integer')
 @click.option('--topic', prompt='Enter the topic to track in twitter',
               help='Must be an string with a real topic')
-# def main(limit, topic):
 
-def main(topic):
+def main(topic, tweet_limit):
 
-    tweet_listener = TweetsListener()
+    tweet_listener = TweetsListener(tweet_limit)
     twitter_stream = Stream(auth, tweet_listener, tweet_mode="extended_tweet")
-    twitter_stream.filter(track=topic)
+    twitter_stream.filter(track=[topic], languages=["en"])
 
 if __name__ == "__main__":
     main()
